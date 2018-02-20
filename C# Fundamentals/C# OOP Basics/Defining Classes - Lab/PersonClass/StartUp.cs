6815a2c31414e1e47948af2662a19de2e2a67a5d
offdge <offdge@abv.bg>
2018-02-20 16:21:48 +0200

    update

--- C# Fundamentals/C# OOP Basics/Defining Classes - Lab/PersonClass/StartUp.cs
+++ C# Fundamentals/C# OOP Basics/Defining Classes - Lab/PersonClass/StartUp.cs
@@ -5,10 +5,8 @@ public class StartUp
     static void Main(string[] args)
     {
         var acc = new Person();
-
         acc.Name = "Pesho";
         acc.Age = 15;
-
         Console.WriteLine($"Name {acc.Name}, age {acc.Age}");
     }
 }
