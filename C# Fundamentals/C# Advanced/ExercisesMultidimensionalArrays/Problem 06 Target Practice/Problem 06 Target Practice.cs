b7d00971cb8aeb9cfa7bde736b600135c8918b87
offdge <35779718+offdge@users.noreply.github.com>
2018-02-20 15:35:57 +0200

    Add files via upload

---
+++ C# Fundamentals/C# Advanced/ExercisesMultidimensionalArrays/Problem 06 Target Practice/Problem 06 Target Practice.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_06_Target_Practice
+{
+    class Program
+    {
+        static void Main()
+        {
+            var matrixSize = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var snake = Console.ReadLine().Trim().ToArray(); ;
+            var shotparameters = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var impactRow = shotparameters[0];
+            var impactColumn = shotparameters[1];
+            var radius = shotparameters[2];
+            var rows = matrixSize[0];
+            var columns = matrixSize[1];
+            var matrix = new char[rows, columns];
+            var matrixFinal = new char[rows, columns];
+            var check = true;
+            var counter = 0;
+            ///////////Matrix fill
+            for (int row = rows - 1; row >= 0; row--)
+            {
+                if (check == true)
+                {
+                    for (int col = columns - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = snake[counter++ % snake.Length];
+                    }
+                    check = false;
+                }
+                else
+                {
+                    for (int col = 0; col < columns; col++)
+                    {
+                        matrix[row, col] = snake[counter++ % snake.Length];
+                    }
+                    check = true;
+                }
+            }
+            ///////////Impact Zone
+            if (impactRow < rows && impactColumn < columns)
+            {
+                for (int i = 0; i <= radius; i++)
+                {
+                    for (int j = 0; j <= radius - i; j++)
+                    {
+                        if (impactRow - i >= 0)
+                        {
+                            if (impactColumn + j < columns)
+                            {
+                                matrix[impactRow - i, impactColumn + j] = ' ';
+                            }
+                            if (impactColumn - j >= 0)
+                            {
+                                matrix[impactRow - i, impactColumn - j] = ' ';
+                            }
+                        }
+                        if (impactRow + i < rows)
+                        {
+                            if (impactColumn + j < columns)
+                            {
+                                matrix[impactRow + i, impactColumn + j] = ' ';
+                            }
+                            if (impactColumn - j >= 0)
+                            {
+                                matrix[impactRow + i, impactColumn - j] = ' ';
+                            }
+                        }
+                    }
+                }
+            }
+            ///////////MatrixFinal fill
+            for (int column = 0; column < columns; column++)
+            {
+                var fill = new List<char>();
+                for (int row = rows - 1; row >= 0; row--)
+                {
+                    if (matrix[row, column] != ' ')
+                    {
+                        fill.Add(matrix[row, column]);
+                    }
+                }
+                for (int i = 0; i < rows; i++)
+                {
+                    if (i < fill.Count)
+                    {
+                        matrixFinal[rows - 1 - i, column] = fill[i];
+                    }
+                    else
+                    {
+                        matrixFinal[rows - 1 - i, column] = ' ';
+                    }
+                }
+            }
+            ////////////Result
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    Console.Write(matrixFinal[row, col] + "");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/C# Advanced/ExercisesMultidimensionalArrays/Problem 07 Lego Blocks/Problem 07 Lego Blocks.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem_07_Lego_Blocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var firstJaggedArray    = new int[n][];
+            var secondJaggedArray   = new int[n][];
+            var check = true;
+            for (int j = 0; j < n; j++)
+            {
+                firstJaggedArray[j]     = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            }
+            for (int k = 0; k < n; k++)
+            {
+                secondJaggedArray[k]    = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                var count = firstJaggedArray[0].Length + secondJaggedArray[0].Length;
+
+                if (firstJaggedArray[i].Length + secondJaggedArray[i].Length != count)
+                {
+                    var totalCells = 0;
+                    for (int l = 0; l < n; l++)
+                    {
+                        totalCells += firstJaggedArray[l].Length + secondJaggedArray[l].Length;
+                    }
+                    Console.WriteLine("The total number of cells is: {0}", totalCells);
+                    check = false;
+                }
+            }
+            if (check == true)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("[" + string.Join(", ", firstJaggedArray[i]) + ", " + string.Join(", ", secondJaggedArray[i].Reverse()) + "]");
+                }
+            }
+        }
+    }
+}
