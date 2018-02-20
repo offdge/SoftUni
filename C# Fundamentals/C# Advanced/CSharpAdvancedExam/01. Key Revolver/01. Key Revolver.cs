53c4220c7e1fbbccf36afe4e86f5ecbb769c131f
offdge <35779718+offdge@users.noreply.github.com>
2018-02-20 15:25:05 +0200

    Add files via upload

---
+++ C# Fundamentals/C# Advanced/CSharpAdvancedExam/01. Key Revolver/01. Key Revolver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Key_Revolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bulletPrice     = int.Parse(Console.ReadLine());
+            var gunBarrelSize   = int.Parse(Console.ReadLine());
+            var bullets         = new Stack<int>(Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            var locks           = new Queue<int>(Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            var valueOftheIntelligence = int.Parse(Console.ReadLine());
+            var bulletsCount = bullets.Count;
+            var countReload = 0;
+
+            for (int i = 0; i < bulletsCount; i++)
+            {
+                if (bullets.Pop() <= locks.Peek())
+                {
+                    Console.WriteLine("Bang!");
+                    locks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+                if (++countReload % gunBarrelSize == 0)
+                {
+                    if (bullets.Count > 0)
+                    {
+                        Console.WriteLine("Reloading!");
+                    }
+                }
+                if (locks.Count == 0)
+                {
+                    Console.WriteLine("{0} bullets left. Earned ${1}", bullets.Count, valueOftheIntelligence - ((bulletsCount - bullets.Count) * bulletPrice));
+                    break;
+                }
+            }
+            if (locks.Count > 0)
+            {
+                Console.WriteLine("Couldn't get through. Locks left: {0}", locks.Count);
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/C# Advanced/CSharpAdvancedExam/04. Hit List/04. Hit List.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Hit_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myDict = new Dictionary<string, Dictionary<string, string>>();
+            var targetInfoindex = int.Parse(Console.ReadLine());
+            var input = Console.ReadLine();
+            //////////Dictionary Fill
+            while (input != "end transmissions")
+            {
+                var firstSplit = input.Split(new[] { '=' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+                var secondSplit = firstSplit[1].Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+                var name = firstSplit[0];
+
+                if (!myDict.ContainsKey(name))
+                {
+                    myDict.Add(name, new Dictionary<string, string>());
+                }
+                foreach (var item in secondSplit)
+                {
+                    var thirdSplit = item.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+                    var infoKey = thirdSplit[0];
+                    var infoValue = thirdSplit[1];
+
+                    if (myDict[name].ContainsKey(infoKey))
+                    {
+                        myDict[name][infoKey] = infoValue;
+                    }
+                    else
+                    {
+                        myDict[name].Add(infoKey, infoValue);
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            var killInput = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            //////////Output
+            Console.WriteLine("Info on {0}:", killInput[1]);
+
+            foreach (var item in myDict.Where( x => x.Key == killInput[1]))
+            {
+                var keyLengthCount = 0;
+                foreach (var element in item.Value.OrderBy(x => x.Key))
+                {
+                    Console.WriteLine("---{0}: {1}", element.Key, element.Value);
+                    keyLengthCount += element.Key.Length + element.Value.Length;
+                }
+                
+                Console.WriteLine("Info index: {0}", keyLengthCount);
+                
+                if (keyLengthCount > targetInfoindex)
+                {
+                    Console.WriteLine("Proceed");
+                }
+                else
+                {
+                    Console.WriteLine("Need {0} more info.", targetInfoindex - keyLengthCount);
+                }
+            }
+        }
+    }
+}
