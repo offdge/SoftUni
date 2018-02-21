c28a0fd2ef4d1073901993f64a388fa9a4e81ebc
offdge <offdge@abv.bg>
2018-02-21 09:55:55 +0200

    update

---
+++ Tech Module/Programming Fundamentals/Exams/Softuni Coffee Orders/SoftuniCoffeeOrders.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace SoftuniCoffeeOrders
+{
+    class Program
+    {
+        static void Main()
+        {
+            var countOfOrders = int.Parse(Console.ReadLine());
+            decimal totalPrice = 0;
+            
+            for (int i = 0; i < countOfOrders; i++)
+            {
+                var pricePerCapsule = decimal.Parse(Console.ReadLine());
+                var orderDate = Console.ReadLine().Split('/').Select(s => int.Parse(s)).ToArray();
+                var capsulesCount = int.Parse(Console.ReadLine());
+                var year = orderDate[2];
+                var month = orderDate[1];
+                var daysInMonth = DateTime.DaysInMonth(year, month);
+                var price = pricePerCapsule * daysInMonth * capsulesCount ;
+
+                Console.WriteLine("The price for the coffee is: ${0:f2}", price);
+
+                totalPrice += price;
+            }
+            Console.WriteLine("Total: ${0:f2}", totalPrice);
+		}
+	}
+}
