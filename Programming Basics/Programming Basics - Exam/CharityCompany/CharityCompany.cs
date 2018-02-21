370ecf2280fb2ef7d2c965bb526d9e6646a13018
offdge <offdge@abv.bg>
2018-02-21 10:28:46 +0200

    update

---
+++ Programming Basics/Programming Basics - Exam/CharityCompany/CharityCompany.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CharityCompany
+{
+    class CharityCompany
+    {
+        static void Main(string[] args)
+        {
+            var days = int.Parse(Console.ReadLine());
+            var peoples = int.Parse(Console.ReadLine());
+            var cakeCount = int.Parse(Console.ReadLine());
+            var gofretCount = int.Parse(Console.ReadLine());
+            var pancakeCount = int.Parse(Console.ReadLine());
+
+            var cakePrise = 45;
+            var gofretPrise = 5.80;
+            var pancakePrise = 3.20;
+
+            var sum = (cakeCount * cakePrise + gofretCount * gofretPrise + pancakeCount * pancakePrise) * peoples;
+            var sumDays = sum * days;
+            var totalSum = sumDays * 0.875;
+
+            Console.WriteLine("{0:f2}", totalSum);
+        }
+    }
+}
---
+++ Programming Basics/Programming Basics - Exam/Triangle/Triangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Triangle
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}", new string('#', (4 * n) + 1));
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                if (i == n / 2 + 1)
+                {
+                    Console.WriteLine("{0}{1}{2}(@){2}{1}{0}",
+                    new string('.', i),
+                    new string('#', (((4 * n) + 1) - (2 * i - 1) - 2 * i) / 2),
+                    new string(' ', (i - 2)));
+                }
+                else
+                {
+                    Console.WriteLine("{0}{1}{2}{1}{0}",
+                    new string('.', i),
+                    new string('#', (((4 * n) + 1) - (2 * i - 1) - 2 * i) / 2),
+                    new string(' ', (2 * i - 1)));
+                }
+            }
+
+            for (int j = 1; j <= n; j++)
+            {
+                Console.WriteLine("{0}{1}{0}",
+                    new string('.', n + j),
+                    new string('#', ((4 * n) + 1) - 2 * (n + j)));
+            }
+        }
+    }
+}
