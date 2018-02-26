aa22c935598d5d3a077d5957b268bca238d07a0d
offdge <offdge@abv.bg>
2018-02-26 16:38:58 +0200

    update

---
+++ C# Fundamentals/C# OOP Basics/Defining Classes - Exercise/CompanyRoster/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class Employee
+{
+
+    private string name;
+    private decimal salary;
+    private string position;
+    private string department;
+    private string email;
+    private int age;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public decimal Salary
+    {
+        get { return salary; }
+        set { salary = value; }
+    }
+    public string Position
+    {
+        get { return position; }
+        set { position = value; }
+    }
+    public string Department
+    {
+        get { return department; }
+        set { department = value; }
+    }
+    public string Email
+    {
+        get { return email; }
+        set { email = value; }
+    }
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public Employee(string name, decimal salary, string position, string department, string email = "n/a", int age = -1)
+    {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+        this.email = email;
+        this.age = age;
+    }
+
+}
+
---
+++ C# Fundamentals/C# OOP Basics/Defining Classes - Exercise/CompanyRoster/StartUp.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class StartUp
+{
+    static void Main()
+    {
+        var employees = new Dictionary<string, List<Employee>>();
+        var n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            var input = Console.ReadLine().Split();
+            var name = input[0];
+            var salary = decimal.Parse(input[1]);
+            var position = input[2];
+            var department = input[3];
+            var email = "n/a";
+            var age = -1;
+
+            if (input.Length == 5)
+            {
+                email = input[4];
+            }
+            if (input.Length == 6)
+            {
+                email = input[4];
+                age = int.Parse(input[5]);
+            }
+
+            var employee = new Employee(name,  salary,  position,  department,  email, age);
+
+            if (!employees.ContainsKey(department))
+            {
+                employees.Add(department, new List<Employee>());
+            }
+            employees[department].Add(employee);
+        }
+
+        //foreach (var employee in employees.OrderByDescending(x => x.Salary).Where(x => x.Department == highestSalaryDepartment))
+        //{
+        //    Console.WriteLine("{0} {1:f2} {2} {3}", employee.Name, employee.Salary, employee.Email, employee.Age);
+        //}
+
+        //foreach (var employee in employees.OrderByDescending(x => x.Value.OrderByDescending(y => y.Salary)))
+        foreach (var employee in employees)
+            {
+            Console.WriteLine(employee.Key);
+
+            foreach (var mem in employee.Value)
+            {
+                Console.WriteLine("{0} {1:f2} {2} {3}", mem.Name, mem.Salary, mem.Email, mem.Age);
+            }
+        }
+
+        //var testSalary = employees.Average(x => x.Salary);
+    }
+
+    //public void Addemployee(string department, Employee employee, Dictionary<string, List<Employee>> employees)
+    //{
+    //    if (!employees.ContainsKey(department))
+    //    {
+    //        employees.Add(department, new List<Employee>());
+    //    }
+    //
+    //    employees[department].Add(employee);
+    //    
+    //}
+}
+
+
