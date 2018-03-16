0bd05fc0d29049b38fd5ee93ac453be486a25b2c
offdge <offdge@abv.bg>
2018-03-16 08:20:10 +0200

    Update

---
+++ C# Fundamentals/C# OOP Basics/Exams/Minedraft/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Minedraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
