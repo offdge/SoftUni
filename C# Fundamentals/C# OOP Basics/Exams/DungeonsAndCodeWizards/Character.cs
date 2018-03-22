2cd79576374fa6dccd7f9cd7e2c3b7dcd3c24bb8
offdge <offdge@abv.bg>
2018-03-22 16:02:07 +0200

    Update

---
+++ C# Fundamentals/C# OOP Basics/Exams/DungeonsAndCodeWizards/Character.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsAndCodeWizards
+{
+    public abstract class Character
+    {
+        private string name;
+        private double baseHealth ;
+        private double health;
+        private double baseArmor;
+        private double armor;
+        private double abilityPoints;
+        private Bag bag;
+        private Faction faction;
+        public bool IsAlive = true;
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public double BaseHealth 
+        {
+            get { return baseHealth ; }
+            private set { baseHealth  = value; }
+        }
+
+        public double Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+        public double BaseArmor
+        {
+            get { return baseArmor; }
+            private set { baseArmor = value; }
+        }
+
+        public double Armor
+        {
+            get { return armor; }
+            private set { armor = value; }
+        }
+
+        public double AbilityPoints
+        {
+            get { return abilityPoints; }
+            private set { abilityPoints = value; }
+        }
+
+        public Bag Bag
+        {
+            get { return bag; }
+            private set { bag = value; }
+        }
+
+        public Faction Faction
+        {
+            get { return faction; }
+            private set { faction = value; }
+        }
+
+        protected virtual double RestHealMultiplier => (double)1 / 5;
+
+        public void TakeDamage(double hitPoints)
+        {
+            if (IsAlive)
+            {
+                if (armor > hitPoints)
+                {
+
+                }
+            }
+            if (!IsAlive)
+            {
+                throw new InvalidOperationException("Must be alive to perform this action!");
+            }
+        }
+
+        public void  Rest()
+        {
+            if (!IsAlive)
+            {
+                throw new InvalidOperationException("Must be alive to perform this action!");
+            }
+        }
+
+        public void UseItem(Item item)
+        {
+            if (!IsAlive)
+            {
+                throw new InvalidOperationException("Must be alive to perform this action!");
+            }
+        }
+
+        public void UseItemOn(Item item, Character character)
+        {
+            if (!character.IsAlive)
+            {
+                throw new InvalidOperationException("Must be alive to perform this action!");
+            }
+        }
+
+        public void GiveCharacterItem(Item item, Character character)
+        {
+            if (!character.IsAlive)
+            {
+                throw new InvalidOperationException("Must be alive to perform this action!");
+            }
+        }
+
+        public void ReceiveItem(Item item)
+        {
+            if (!IsAlive)
+            {
+                throw new InvalidOperationException("Must be alive to perform this action!");
+            }
+        }
+
+        public Character(string name, double health, double armor, double abilityPoints, Bag bag, Faction faction)
+        {
+            this.Name = name;
+            this.Health = health;
+            this.Armor = armor;
+            this.AbilityPoints = abilityPoints;
+            this.Bag = bag;
+            this.Faction = faction;
+        }
+    }
+}
---
+++ C# Fundamentals/C# OOP Basics/Exams/DungeonsAndCodeWizards/HealthPotion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsAndCodeWizards
+{
+    public class HealthPotion : Item
+    {
+        public HealthPotion() : base(5)
+        {
+        }
+
+        public void AffectCharacter(Character character)
+        {
+            if (!character.IsAlive)
+            {
+                throw new InvalidOperationException("Must be alive to perform this action!");
+            }
+            character.Health += 20;
+        }
+    }
+}
