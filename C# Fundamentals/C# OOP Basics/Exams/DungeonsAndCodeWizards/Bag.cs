af4d4d1725be005285588494c8579903e7e8dc24
offdge <offdge@abv.bg>
2018-03-22 21:32:55 +0300

    update

---
+++ C# Fundamentals/C# OOP Basics/Exams/DungeonsAndCodeWizards/Bag.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsAndCodeWizards
+{
+    public abstract class Bag
+    {
+        public Bag(int capacity)
+        {
+            this.capacity = capacity;
+        }
+        private int capacity;
+
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+
+        private readonly List<Item> items;
+
+        public IReadOnlyCollection<Item> Items
+        {
+            get
+            {
+                return this.items.AsReadOnly();
+            }
+        }
+
+        private int load;
+
+        public int Load
+        {
+            get { return load; }
+            set { load = value; }
+        }
+
+        public void AddItem(Item item)
+        {
+            if (this.load + item.Weight > this.capacity)
+            {
+                throw new InvalidOperationException("Bag is full!");
+            }
+            items.Add(item);
+        }
+
+        public Item GetItem (string name)
+        {
+           if (items.Count == 0)
+           {
+               throw new InvalidOperationException("Bag is empty!");
+           }
+           if (!items.Any(x=>x.GetType().Name == name))
+           {
+               throw new ArgumentException($"No item with name {name} in bag!");
+           }
+
+            var item = this.items.First(x => x.GetType().Name == name);
+            this.items.Remove(item);
+            return item;
+        }
+    }
+}
---
+++ C# Fundamentals/C# OOP Basics/Exams/DungeonsAndCodeWizards/Cleric.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsAndCodeWizards
+{
+    public class Cleric : Character, IHealable
+    {
+        public Cleric(string name, Faction faction) : base(name, health: 50, armor: 25, abilityPoints: 40, bag: new Backpack(), faction: faction)
+        {
+
+        }
+        public void Heal(Character character)
+        {
+
+        }
+
+        protected override double RestHealMultiplier => 0.5;
+    }
+}
