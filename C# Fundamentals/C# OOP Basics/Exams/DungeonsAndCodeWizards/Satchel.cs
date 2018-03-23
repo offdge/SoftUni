c417883caecf46733720c126193c604962c05d5a
offdge <offdge@abv.bg>
2018-03-23 08:57:17 +0200

    Update

---
+++ C# Fundamentals/C# OOP Basics/Exams/DungeonsAndCodeWizards/Satchel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsAndCodeWizards
+{
+    public class Satchel : Bag
+    {
+        public Satchel() : base(20)
+        {
+        }
+    }
+}
---
+++ C# Fundamentals/C# OOP Basics/Exams/DungeonsAndCodeWizards/Warrior.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsAndCodeWizards
+{
+    public class Warrior : Character , IAttackable
+    {
+        public Warrior(string name, Faction faction) : base(name, health: 100, armor: 50, abilityPoints: 40, bag: new Satchel(), faction: faction)
+        {
+
+        }
+        public void Attack(Character character)
+        {
+
+        }
+    }
+}
