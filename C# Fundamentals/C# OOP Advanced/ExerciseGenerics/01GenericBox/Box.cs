df7bf67ee0e8f4faf9f527b56ac0a5577a1e441b
offdge <offdge@abv.bg>
2018-03-29 16:44:14 +0300

    Update

---
+++ C# Fundamentals/C# OOP Advanced/ExerciseGenerics/01GenericBox/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01GenericBox
+{
+    public class Box<T>
+    {
+        private List<T> box;
+
+        public T Value { get; set; }
+
+        public Box()
+        {
+            this.box = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            this.box.Add(item);
+            this.Value = item;
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T).FullName}: {this.Value}";
+        }
+    }
+}
---
+++ C# Fundamentals/C# OOP Advanced/ExerciseGenerics/01GenericBox/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01GenericBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var box = new Box<int>();
+
+            box.Add(1234);
+
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
