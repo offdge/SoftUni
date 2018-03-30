564c188a6536141fdf9751f1ba3d952fc0f560d3
offdge <offdge@abv.bg>
2018-03-30 16:46:19 +0300

    update

---
+++ C# Fundamentals/C# OOP Advanced/ExerciseGenerics/03GenericSwapMethodStrings/Box.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _01GenericBox
+{
+    public class Box<T> : IEnumerable
+    {
+        private List<T> box;
+
+        public T Value { get; private set; }
+
+        public Box()
+        {
+            this.box = new List<T>();
+
+        }
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public void Add(T value)
+        {
+            this.box.Add(value);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this.box.GetEnumerator();
+        }
+
+        public void Print()
+        {
+            foreach (var item in box)
+            {
+                Console.WriteLine($"{item.GetType().FullName}: {item}");
+            }
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            var temp = box[firstIndex];
+            box[firstIndex] = box[secondIndex];
+            box[secondIndex] = temp;
+        }
+    }
+}
---
+++ C# Fundamentals/C# OOP Advanced/ExerciseGenerics/04GenericSwapMethodIntegers/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01GenericBox
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var box = new Box<int>();
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+
+                box.Add(input); 
+            }
+
+            var commandSwap = Console.ReadLine().Split();
+            var firstIndex = int.Parse(commandSwap[0]);
+            var secondIndex = int.Parse(commandSwap[1]);
+
+            box.Swap(firstIndex, secondIndex);
+
+            box.Print();
+        }  
+    }
+}
